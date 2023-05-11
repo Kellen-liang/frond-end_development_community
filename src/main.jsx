4a2fe26735703b4a504b37d7006f8831d60d18c0
kellen-liang <dh18344094637@outlook.com>
2023-05-11 21:58:24 +0800

    修复netlify部署图片报错问题0.0.7

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'normalize.css'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import axios from 'axios'
 import { AuthContextProvider } from './context/authContext.jsx'
 // axios.defaults.withCredentials = true
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <AuthContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </AuthContextProvider>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
